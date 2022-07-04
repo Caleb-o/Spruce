3cf3bee4837d5d8ab218394e88d2bc21cee13ec5
Caleb <CalebOttoGameDev@Gmail.com>
2022-07-04 23:33:28 +1000

    test: Move test sources into their own folders

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ pub fn run(filename: String) -> Result<(), SpruceError> {
 #[test]
 fn valid_tokens() {
 	use lexing::{lexer::Lexer, token::TokenKind};
-	let mut lexer = Lexer::new(fs::read_to_string("./testsrc/valid_tokens.sp").unwrap());
+	let mut lexer = Lexer::new(fs::read_to_string("./testsrc/lexer/valid_tokens.sp").unwrap());
     
 	loop {
 		match lexer.next() {
@@ -47,7 +47,7 @@ fn valid_tokens() {
 
 #[test]
 fn test_constant_basic() {
-	let mut parser = Parser::new(fs::read_to_string("./testsrc/constant_basic.sp").unwrap());
+	let mut parser = Parser::new(fs::read_to_string("./testsrc/parser/constant_basic.sp").unwrap());
 	match parser.parse() {
 		Ok(p) => assert_eq!(p.to_sexpr(), "((const foo (fn ()()))(const bar 120)(const baz My String)(const number (+ 100 (* 200 3))))"),
 		Err(e) => panic!("{e}"),
@@ -56,7 +56,7 @@ fn test_constant_basic() {
 
 #[test]
 fn test_constant_operation() {
-	let mut parser = Parser::new(fs::read_to_string("./testsrc/constant_operation.sp").unwrap());
+	let mut parser = Parser::new(fs::read_to_string("./testsrc/parser/constant_operation.sp").unwrap());
 	match parser.parse() {
 		Ok(p) => assert_eq!(p.to_sexpr(), "((const foo (+ 200 300))(const bar 120)(const baz (+ bar (* foo (- 1)))))"),
 		Err(e) => panic!("{e}"),
@@ -65,7 +65,7 @@ fn test_constant_operation() {
 
 #[test]
 fn test_constant_range() {
-	let mut parser = Parser::new(fs::read_to_string("./testsrc/constant_range.sp").unwrap());
+	let mut parser = Parser::new(fs::read_to_string("./testsrc/parser/constant_range.sp").unwrap());
 	match parser.parse() {
 		Ok(p) => assert_eq!(p.to_sexpr(), "((const foo 10)(const bar 5)(const range foo..(* bar bar)))"),
 		Err(e) => panic!("{e}"),
@@ -74,7 +74,7 @@ fn test_constant_range() {
 
 #[test]
 fn test_function_def_and_call() {
-	let mut parser = Parser::new(fs::read_to_string("./testsrc/function_def_and_call.sp").unwrap());
+	let mut parser = Parser::new(fs::read_to_string("./testsrc/parser/function_def_and_call.sp").unwrap());
 	match parser.parse() {
 		Ok(p) => assert_eq!(p.to_sexpr(), "((const foo (fn (a, b)()))(call foo(10, 20))"),
 		Err(e) => panic!("{e}"),
