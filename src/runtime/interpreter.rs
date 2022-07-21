1718be48b94530de27ff7f3d8cb7dd790e3bab17
Caleb <CalebOttoGameDev@Gmail.com>
2022-07-22 01:53:12 +1000

    interpreter: Simple assignments

--- src/runtime/interpreter.rs
+++ src/runtime/interpreter.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::parsing::ast::{Node, AST, Body, VariableDeclaration, Println, FunctionDefinition, FunctionCall};
+use crate::parsing::ast::{Node, AST, Body, VariableDeclaration, Println, FunctionDefinition, FunctionCall, VariableAssign};
 
 use super::{Value, FnValue};
 
@@ -34,7 +34,7 @@ impl Interpreter {
 			AST::FunctionDefinition(ref fndef) => self.visit_fn_def(fndef),
 			AST::FunctionCall(ref call) => self.visit_call(call),
 			AST::VariableDeclaration(ref decl) => self.visit_var_decl(decl),
-			AST::VariableAssign(_) => todo!(),
+			AST::VariableAssign(ref assign) => self.visit_var_assign(assign),
 			AST::Identifier(ref id) => self.visit_identifier(id),
 			AST::Number(num) => Value::Number(num),
 			AST::String(ref string) => Value::String(string.clone()),
@@ -93,6 +93,14 @@ impl Interpreter {
 		self.variables.last_mut().unwrap().insert(decl.identifier.clone(), expr_value);
 
 		Value::Unit
+	}
+
+	fn visit_var_assign(&mut self, assign: &VariableAssign) -> Value {
+		let expr_value = self.visit(&assign.expression);
+		// FIXME: Add assignments to outer scopes
+		self.variables.last_mut().unwrap().insert(assign.identifier.clone(), expr_value.clone());
+
+		expr_value
 	}
 
 	fn visit_identifier(&mut self, id: &String) -> Value {
