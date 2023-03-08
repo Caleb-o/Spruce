618a2c5f26f96d602871eb4be16f5e585e26c565
Caleb <caleb.ottohayes@gmail.com>
2023-03-08 23:48:42 +1000

    Added clap to handle command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -11,62 +11,62 @@ mod compiler;
 mod vm;
 mod stepper;
 
-use std::{env, fs, rc::Rc};
+use std::{fs, rc::Rc};
 use compiler::Compiler;
 use environment::Environment;
 use parser::Parser;
 use stepper::Stepper;
 use vm::VM;
 
+use clap::Parser as ClapParser;
+
+#[derive(ClapParser)] // requires `derive` feature
+#[command(name = "spruce")]
+#[command(bin_name = "spruce")]
+enum SpruceCli {
+    #[command(short_flag='d')]
+    Dump(RunArgs),
+    #[command(short_flag='r')]
+    Run(RunArgs),
+    #[command(short_flag='s')]
+    Step { path: String },
+}
+
+#[derive(clap::Args)]
+#[command(author, version, about, long_about = None)]
+struct RunArgs {
+    path: String,
+    #[clap(default_value_t=false, short, long)]
+    script_mode: bool,
+}
+
 fn main() {
-    let args = env::args().collect::<Vec<_>>();
-
-    if args.len() != 3 {
-        println!("Usage: spruce [debug|run] script");
-        return;
-    }
-
-    match &args[1][0..] {
-        "d" | "dump" => {
-            match compile(true, &args[2]) {
-                Ok(mut e) => {
-                    e.dump();
-                }
-                Err(e) => eprintln!("{e}"),
-            }
-        }
-        "s" | "step" => {
-            match compile(true, &args[2]) {
-                Ok(e) => Stepper::new(e).run(),
-                Err(e) => eprintln!("{e}"),
-            }
-        }
-        "r" | "run" => {
-            if let Ok(ref source) = fs::read_to_string(&args[2]) {
-                match compile(false, source) {
+    match SpruceCli::parse() {
+        SpruceCli::Run(args) | SpruceCli::Dump(args) => {
+            if let Ok(source) = fs::read_to_string(&args.path) {
+                match compile(source) {
                     Ok(env) => VM::new(env).run(),
                     Err(e) => eprintln!("{e}"),
                 }
             } else {
-                eprintln!("Could not load file '{}'", args[2]);
-                return;
+                eprintln!("Could not load file '{}'", args.path);
             }
         }
-        _ => {
-            println!("Unknown sub-command '{}'", args[1]);
-            return;
-        },
+        SpruceCli::Step { path } => {
+            if let Ok(source) = fs::read_to_string(&path) {
+                match compile(source) {
+                    Ok(e) => Stepper::new(e).run(),
+                    Err(e) => eprintln!("{e}"),
+                }
+            } else {
+                eprintln!("Could not load file '{}'", path);
+            }
+        }
     }
 }
 
-fn compile(is_file: bool, source: &String) -> Result<Box<Environment>, String> {
-    let source = if is_file {
-        match fs::read_to_string(source) {
-            Ok(s) => Rc::new(s),
-            Err(e) => return Err(e.to_string()),
-        }
-    } else { Rc::new(source.clone()) };
-    
+fn compile(source: String) -> Result<Box<Environment>, String> {
+    let source = Rc::new(source);
     let mut parser = match Parser::new(Rc::clone(&source)) {
         Ok(c) => c,
         Err(e) => return Err(e.to_string()),
