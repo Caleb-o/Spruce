56a18e1ed5ab778db71fd6ee1a7a3ac905ad9eea
Caleb <caleb.ottohayes@gmail.com>
2023-03-10 23:26:31 +1000

    Show compilation time when completing successfully

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{rc::Rc, time::Instant};
 
 use crate::{compiler::Compiler, source::Source, parser::Parser, environment::Environment, ast::Ast, error::SpruceErr};
 
@@ -17,11 +17,15 @@ pub fn run_parser(file_path: String, source: String, script_mode: bool) -> Resul
 }
 
 pub fn compile_source(file_path: String, source: String, script_mode: bool) -> Result<Box<Environment>, SpruceErr> {
+    let timer = Instant::now();
     let (source, program) = run_parser(file_path, source, script_mode)?;
     
     let mut compiler = Compiler::new(source);
     match compiler.run(program, script_mode) {
-        Ok(env) => Ok(env),
+        Ok(env) => {
+            println!("\x1b[32mFinished\x1b[0m Compilation took {:.3} seconds", timer.elapsed().as_secs_f32());
+            Ok(env)
+        },
         Err(e) => Err(e.into()),
     }
 }
